a4c918ca441cf78423f66bdc4eba4f9228537e70
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-20 13:52:35 +0300

    Mission Passed

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  const handleClick = () => {
+  setIsOn ((prevState) =>!prevState)
+  }
+
+  return (
+    <button onClick={handleClick}>
+      {isOn ? 'ON' : 'OFF'}
+    </button>
+  )
 }
 
 export default Toggle;
